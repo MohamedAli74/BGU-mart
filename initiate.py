a3712c8b4364a19362596598b8e556faa47c01f9
MohamedAli74 <mohamedali243.123@gmail.com>
2025-02-07 17:23:16 +0200

    Implemented initiate.py

--- initiate.py
+++ initiate.py
@@ -1,41 +1,38 @@
 from persistence import *
-
 import sys
 import os
+from typing import List
 
-def add_branche(splittedline : list[str]):
-    #TODO: add the branch into the repo
-    pass
+def add_branche(splittedline: List[str]):
+    repo.branches.insert(Branche(*splittedline))
 
-def add_supplier(splittedline : list[str]):
-    #TODO: insert the supplier into the repo
-    pass
+def add_supplier(splittedline: List[str]):
+    repo.suppliers.insert(Supplier(*splittedline))
 
-def add_product(splittedline : list[str]):
-    #TODO: insert product
-    pass
+def add_product(splittedline: List[str]):
+    repo.products.insert(Product(*splittedline))
 
-def add_employee(splittedline : list[str]):
-    #TODO: insert employee
-    pass
+def add_employee(splittedline: List[str]):
+    repo.employees.insert(Employee(*splittedline))
 
-adders = {  "B": add_branche,
-            "S": add_supplier,
-            "P": add_product,
-            "E": add_employee}
+adders = {
+    "B": add_branche,
+    "S": add_supplier,
+    "P": add_product,
+    "E": add_employee
+}
 
-def main(args : list[str]):
+def main(args: list[str]):
     inputfilename = args[1]
     # delete the database file if it exists
     repo._close()
-    # uncomment if needed
-    # if os.path.isfile("bgumart.db"):
-    #     os.remove("bgumart.db")
+    if os.path.isfile("bgumart.db"):
+        os.remove("bgumart.db")
     repo.__init__()
     repo.create_tables()
     with open(inputfilename) as inputfile:
         for line in inputfile:
-            splittedline : list[str] = line.strip().split(",")
+            splittedline: list[str] = line.strip().split(",")
             adders.get(splittedline[0])(splittedline[1:])
 
 if __name__ == '__main__':
