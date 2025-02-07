d27f57cc80e1f1e7e3966f39ba338f528ba016c5
MohamedAli74 <mohamedali243.123@gmail.com>
2025-02-07 17:24:49 +0200

    Implemented the required logics in action.py

--- action.py
+++ action.py
@@ -1,13 +1,19 @@
 from persistence import *
-
 import sys
 
-def main(args : list[str]):
-    inputfilename : str = args[1]
+def main(args: list[str]):
+    inputfilename: str = args[1]
     with open(inputfilename) as inputfile:
         for line in inputfile:
-            splittedline : list[str] = line.strip().split(", ")
-            #TODO: apply the action (and insert to the table) if possible
+            splittedline: list[str] = line.strip().split(", ")
+            product_id, quantity, activator_id, date = splittedline
+            quantity = int(quantity)
+            product = repo.products.find(id=product_id)[0]
+            if quantity < 0 and product.quantity + quantity < 0:
+                continue
+            repo.activities.insert(Activitie(product_id, quantity, activator_id, date))
+            product.quantity += quantity
+            repo.products.update(product)  # Update the product
 
 if __name__ == '__main__':
     main(sys.argv)
